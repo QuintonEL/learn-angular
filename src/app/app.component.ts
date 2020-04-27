b4a98a24f6eec514261c8168c6fbf0094efa9bdf
QuintonEL <quintonlaborde@gmail.com>
2020-04-27 16:25:47 -0600

    removed animation code and added screenshot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,6 @@ import {trigger, state, style, animate, transition} from '@angular/animations';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [trigger('taskState', [
-    state('inactive', style({opacity: 1, transform: 'translateX(0) scale(1)'})),
-    state('active',   style({opacity: 1, transform: 'translateX(0) scale(1)'})),
-    state('void',   style({opacity: 0, display: 'none', transform: 'translateX(0) scale(1)'})),
-    transition('* => void', [
-        animate('1s 8 ease-out', style({
-            opacity: 0,
-            transform: 'translateX(0) scale(0.5)'
-        }))
-    ])
-  ])]
 })
 export class AppComponent {
   title = 'learn-angular';
